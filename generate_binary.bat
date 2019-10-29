94fcab015b6d601245b9c26df602697e3d3b1c1b
Jake Walker <hi@jakew.me>
2019-10-29 10:50:01 +0000

    Add Windows binary generation script

---
+++ generate_binary.bat
@@ -0,0 +1,9 @@
+@echo off
+echo Updating dependencies...
+python -m pip install --upgrade pip
+python -m pip install pipenv
+python -m pipenv install --dev
+echo Generating binary...
+python -m pipenv run pyinstaller -y --add-data "payloads;payloads/." -F centsecure.py
+echo Complete! Opening Explorer...
+start dist
