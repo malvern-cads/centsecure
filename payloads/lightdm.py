fb95cd6e62504aa46c91503b86626cf0ee720998
13LaneT <13lanet@chase.worcs.sch.uk>
2019-10-30 10:54:57 -0700

    Fixes #2

---
+++ payloads/lightdm.py
@@ -0,0 +1,33 @@
+import configparser
+import shutil
+
+
+class Lightdm(payload.Payload):
+    name = "Lightdm securing"
+    os = ["Ubuntu"]
+    os_version = ["16.04"]
+
+    def __init__(self):
+        print("Lightdm instance created!")
+
+    def execute(self):
+        print("Lightdm has been executed!")
+        path = "/etc/lightdm/lightdm.conf"
+        config = configparser.ConfigParser()
+        try:
+            shutil.copy2(path, ".")
+            print("Backed up {0} to .".format(path))
+            config.read(path)
+        except FileNotFoundError:
+            print("{} not found".format(path))
+        config["SeatDefaults"] = {}
+        config["SeatDefaults"]["user-session"] = "ubuntu"
+        config["SeatDefaults"]["greeter-session"] = "unity-greeter"
+        config["SeatDefaults"]["greeter-show-manual-login"] = "true"
+        config["SeatDefaults"]["greeter-hide-users"] = "true"
+        config["SeatDefaults"]["allow-guest"] = "false"
+
+        with open(path, "w+") as out_file:
+            config.write(out_file)
+        print("{} updated".format(path))
+        print("Lightdm has finished")
