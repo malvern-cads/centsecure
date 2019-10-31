6535acb7e700a1ecf0a1266d4e8a11366aa4daf7
Jake Walker <hi@jakew.me>
2019-10-31 19:51:03 +0000

    Delete generate_binary.bat

--- generate_binary.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Updating dependencies...
-python -m pip install --upgrade pip
-python -m pip install pipenv
-python -m pipenv install --dev
-echo Generating binary...
-python -m pipenv run pyinstaller -y --add-data "payloads;payloads/." -F centsecure.py
-echo Done. Look in the 'dist' folder
