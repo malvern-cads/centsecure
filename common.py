e25eb9dcd9db72bffa57431cc956e2747f52bfad
Jake Walker <hi@jakew.me>
2019-10-31 21:36:25 +0000

    Combine functions and use shutil.copy2()

--- common.py
+++ common.py
@@ -24,25 +24,36 @@ def _filename(path, t=None):
     return ((t + "_") if t is not None else "") + "{}-{}-{}_{}-{}-{}_".format(d.year, d.month, d.day, d.hour, d.minute, d.second) + path.replace("/", "_")[1:]
 
 
-def backup(source):
+def backup(source, compress=False):
     """
     Copy the file at `source` to the backup directory.
     """
-    print("Backing up file {}...".format(source))
-    dest_filename = _filename(source, "file")
-    dest_path = os.path.join(_backup_directory(), dest_filename)
-    shutil.copyfile(source, dest_path)
-    print("The file {} has been backed up to {}".format(source, dest_path))
-    return dest_path
+    # Check if the source path is a folder or not
+    folder = os.path.isdir(source)
+    backup_type = "folder" if folder else "file"
+    print("Backing up {} {}...".format(backup_type, source))
 
-
-def backup_folder(source):
-    """
-    Copy the folder at `source` to the backup directory.
-    """
-    print("Backing up folder {}...".format(source))
-    dest_filename = _filename(source, "folder") + (".tar.bz2" if platform.system().lower() == "linux" else ".zip")
+    # Generate a filename from the source path
+    dest_filename = _filename(source, backup_type)
+    # Append the filename from above onto the end of the backup directory
     dest_path = os.path.join(_backup_directory(), dest_filename)
-    shutil.make_archive(dest_path, ("bztar" if platform.system().lower() == "linux" else "zip"), source)
-    print("The folder {} has been compressed and backed up to {}".format(source, dest_path))
+
+    # If we are backing up a folder and it should be compressed
+    if compress and folder:
+        # Use a .tar.bz2 for Linux (higher compression) or if not use a .zip (lower compression)
+        compress_type = "bztar" if platform.system().lower() == "linux" else "zip"
+        # Get the file extension depending on the compression type
+        file_ext = ".tar.bz2" if compress_type == "bztar" else ".zip"
+        # Add the file extension to the path
+        dest_path += file_ext
+        # Copy the folder to the archive
+        shutil.make_archive(dest_path, compress_type, source)
+    else:
+        if not folder:
+            # Copy the source file to the destination
+            shutil.copy2(source, dest_path)
+        else:
+            # Copy the source folder to the destination
+            shutil.copytree(source, dest_path)
+    print("The {} {} has been{} backed up to {}".format(backup_type, source, (" compressed and" if compress and folder else ""), dest_path))
     return dest_path
