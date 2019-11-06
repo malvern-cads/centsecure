7751056bded17a0247dc4690d9a4745b8e9b24bf
limefax <64359663+limefax@users.noreply.github.com>
2019-11-06 19:13:59 +0000

    Fixed typo

--- payloads/ssh.py
+++ payloads/ssh.py
@@ -36,7 +36,7 @@ class SSH(payload.Payload):
             "PermitUserEnvironment": "no",
             "Ciphers": "chacha20-poly1305@openssh.com,aes256-gcm@openssh.com,aes128-gcm@openssh.com,aes256-ctr,aes192-ctr,aes128-ctr",
             "MACs": "hmac-sha2-512-etm@openssh.com,hmac-sha2-256-etm@openssh.com,hmac-sha2-512,hmac-sha2-256",
-            "KexAlgorithms": "curve25519-sha256,curve25519-sha256@libssh.org,diffie-hellman-group14-sha256,diffie-hellman-group16-sha512,diffie-hellman-group18-sha512,ecdh-sha2-nistp521,ecdh-sha2-nistp384,ecdh-sha2-nistp256,diffie-hellman-group-exchange-sha256",
+            "KeyAlgorithms": "curve25519-sha256,curve25519-sha256@libssh.org,diffie-hellman-group14-sha256,diffie-hellman-group16-sha512,diffie-hellman-group18-sha512,ecdh-sha2-nistp521,ecdh-sha2-nistp384,ecdh-sha2-nistp256,diffie-hellman-group-exchange-sha256",
             "ClientAliveInterval": "300",
             "ClientAliveCountMax": "0",
             "LoginGraceTime": "60",
