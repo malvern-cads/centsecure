05f587c3867bcf4932ee638e64e5e40d2c0fd3f2
13LaneT <13lanet@chase.worcs.sch.uk>
2019-11-06 15:41:05 +0000

    Implementing user.js now works

--- payloads/firefox.py
+++ payloads/firefox.py
@@ -9,15 +9,27 @@ class Firefox(payload.Payload):
     os_version = ["All"]
 
     def execute(self):
-        add_user_js()
+        common.warn("Make sure Firefox has been updated to the latest version (70+) in order for the settting to apply")
+        self.add_user_js()
 
     def add_user_js(self):
         if "Windows" in payload.get_os():
-            profile_dir = "%APPDATA%\\Roaming\\Mozilla\\Firefox\\profiles\\"
+            home_dir = "C:\\Users"
+            profile_dir = "AppData\\Roaming\\Mozilla\\Firefox\\profiles"
         elif "Linux" in payload.get_os():
-            profile_dir = "~/.mozilla/firefox/"
+            home_dir = "/home/"
+            profile_dir = ".mozilla/firefox/"
 
-        profiles = os.listdir()
-        
-        for profile in profiles:
-            shutil.copy("user.js", profile + "/")
+        home_dirs = os.listdir(home_dir)
+
+        for home in home_dirs:
+            current_profile_dir = os.path.join(home_dir, home, profile_dir)
+
+            if os.path.isdir(current_profile_dir):
+                common.info("Adding user.js for {}".format(home))
+                profiles = os.listdir(current_profile_dir)
+            
+                for profile in profiles:
+                    path = os.path.join(current_profile_dir, profile)
+                    if os.path.isdir(path):
+                        shutil.copy("user.js", path)
