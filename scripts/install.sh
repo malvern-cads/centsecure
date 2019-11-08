26404d7833dcb5f91d66131cc41d3a4de634d3bd
Jake Walker <hi@jakew.me>
2019-11-08 19:21:27 +0000

    Corrected Python Version for Linux Installer

--- scripts/install.sh
+++ scripts/install.sh
@@ -13,8 +13,8 @@ echo "Installing dependencies (1/2)..."
 apt install software-properties-common apt-transport-https -y
 add-apt-repository ppa:deadsnakes/ppa
 apt update
-apt install unzip python3.8 python3.8-dev python3.8-distutils curl -y
-curl --silent --show-error --retry 5 https://bootstrap.pypa.io/get-pip.py | sudo python3.8
+apt install unzip python3.7 python3.7-dev python3.7-distutils curl -y
+curl --silent --show-error --retry 5 https://bootstrap.pypa.io/get-pip.py | sudo python3.7
 
 # Unzip file to destination
 echo "Extracting files..."
@@ -24,6 +24,6 @@ cp requirements.txt ${DESTINATION}
 
 cd /opt/centsecure
 echo "Installing dependencies (2/2)..."
-python3.8 -m pip install -r requirements.txt
+python3.7 -m pip install -r requirements.txt
 
-echo "CentSecure has been installed. To run, change directory to '${DESTINATION}' and run the command 'python3.8 centsecure.py'. Good luck!"
+echo "CentSecure has been installed. To run, change directory to '${DESTINATION}' and run the command 'python3.7 centsecure.py'. Good luck!"
