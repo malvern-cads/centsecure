beee16086d140911c9061769e7687edeaab73b8a
limefax <64359663+limefax@users.noreply.github.com>
2019-11-09 12:28:42 +0000

    remove common.run()

--- payloads/applypolicies.py
+++ payloads/applypolicies.py
@@ -14,15 +14,15 @@ class ApplyPolicies(payload.Payload):
         """Execute the payload."""
         common.info("Applying Local Security Policy...")
         cmd = "secedit.exe /configure /db %windir%\\security\\local.sdb /cfg policies\\security_policy.inf"
-        common.stdout(common.run(cmd))
+        os.system(cmd)
 
         # displays access is denies but still runs
         common.info("Applying Firewall Policy...")
         cmd = "netsh advfirewall import \"policies\\firewall.wfw\""
-        common.stdout(common.run(cmd))
+        os.system(cmd)
 
         # TODO doesn't appear in gui but does from cmd??
         # No idea if this actually works...
         common.info("Applying Advanced Audit Policies...")
         cmd = "auditpol /restore /file:policies\\audit.csv"
-        common.stdout(common.run(cmd))
+        os.system(cmd)
