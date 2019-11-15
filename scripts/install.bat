ee718fbb749443f4d33e64fb48ac4c39963948e3
limefax <64359663+limefax@users.noreply.github.com>
2019-11-15 16:09:13 +0000

    Added download git while installing python

--- scripts/install.bat
+++ scripts/install.bat
@@ -11,26 +11,7 @@ IF %ERRORLEVEL% EQU 0 (
     exit /b 1
 )
 
-echo ^> Downloading python...
-powershell $ProgressPreference = 'SilentlyContinue'; [Net.ServicePointManager]::SecurityProtocol = 'tls12, tls11, tls'; wget https://www.python.org/ftp/python/3.7.5/python-3.7.5.exe -OutFile python_install.exe
-
-echo ^> Installing python...
-python_install.exe /passive InstallAllUsers=1 PrependPath=1 Include_doc=0 Include_dev=0 Include_tcltk=0 Include_test=0 Include_tools=0
-
-echo ^> Installing dependencies...
-cd "c:\Program Files (x86)\Python*"
-python.exe -m pip install colorama pypiwin32
-python.exe Scripts/pywin32_postinstall.py -install
-
-echo ^> Python has been installed.
-
-echo ^> GitBash will now be installed
-pause
-
-echo ^> Downloading git for windows...
-powershell $ProgressPreference = 'SilentlyContinue'; [Net.ServicePointManager]::SecurityProtocol = 'tls12, tls11, tls'; wget https://github.com/git-for-windows/git/releases/download/v2.24.0.windows.2/Git-2.24.0.2-64-bit.exe -OutFile git_installer.exe
-
-echo ^> Creating config file...
+echo ^> Creating git config file...
 (
 echo [Setup]
 echo Lang=default
@@ -54,8 +35,30 @@ echo EnableSymlinks=Disabled
 echo EnableBuiltinInteractiveAdd=Disabled
 )>"win_git.inf"
 
-echo ^> Installing git for windows
-git_installer.exe /SP- /SILENT /ALLUSERS /LOADINF="win_git.inf"
+echo ^> Creating git_install.bat...
+(
+echo @echo off
+echo echo ^^^> GitBash will now be installed
+echo echo ^^^> Downloading git for windows...
+echo echo ^^^> You might need to press a key/ctrl-c to update the command prompt in order for the script to continue
+echo powershell $ProgressPreference = 'SilentlyContinue'^; [Net.ServicePointManager]::SecurityProtocol = 'tls12, tls11, tls'^; wget https://github.com/git-for-windows/git/releases/download/v2.24.0.windows.2/Git-2.24.0.2-32-bit.exe -OutFile git_installer.exe
+echo echo ^^^> Installing git for windows
+echo git_installer.exe /SP- /SILENT /ALLUSERS /LOADINF="win_git.inf"
+echo echo ^^^> All done!
+echo pause
+)>"git_install.bat"
 
-echo ^> All done!
-pause
+echo ^> Downloading python...
+powershell $ProgressPreference = 'SilentlyContinue'; [Net.ServicePointManager]::SecurityProtocol = 'tls12, tls11, tls'; wget https://www.python.org/ftp/python/3.7.5/python-3.7.5.exe -OutFile python_install.exe
+
+start git_install.bat
+
+echo ^> Installing python...
+python_install.exe /passive InstallAllUsers=1 PrependPath=1 Include_doc=0 Include_dev=0 Include_tcltk=0 Include_test=0 Include_tools=0
+
+echo ^> Installing dependencies...
+cd "c:\Program Files*\Python*"
+python.exe -m pip install colorama pypiwin32
+;python.exe Scripts/pywin32_postinstall.py -install
+
+echo ^> Python has been installed.
