990f44d5e3d2aecb175cf36faa0ac84e4d1541fd
limefax <64359663+limefax@users.noreply.github.com>
2019-12-06 12:22:13 -0800

    Added removal of system cron

--- plugins/cron.py
+++ plugins/cron.py
@@ -13,6 +13,11 @@ class Cron(plugin.Plugin):
 
     def execute(self):
         """Removes All Cronjobs."""
+        self._remove_user_jobs()
+        self._remove_system_jobs()
+
+    def _remove_user_jobs(self):
+        """Removes all user cronjobs."""
         path = "/var/spool/cron"
         common.backup(path, compress=True)
 
@@ -22,4 +27,24 @@ class Cron(plugin.Plugin):
                 os.unlink(os.path.join(root, f))
             for d in dirs:
                 shutil.rmtree(os.path.join(root, d))
-        common.info("Removed all cronjobs")
+        common.debug("Removed all user cronjobs")
+
+    def _remove_system_jobs(self):
+        """Removes all system cronjobs."""
+        targets = ["/etc/anacrontab", "/etc/crontab", "/etc/cron.d", "/etc/cron.daily", "/etc/cron.hourly", "/etc/cron.monthly", "/etc/cron.weekly", "/var/spool/anacron"]
+        for target in targets:
+            self._remove_and_backup(target)
+
+    def _remove_and_backup(self, path):
+        """Backs up file and removes it.
+
+        Args:
+            path (str): the path to remove
+
+        """
+        if os.path.isfile(path):
+            common.backup(path, compress=True)
+            os.remove(path)  # remove the file
+        elif os.path.isdir(path):
+            common.backup(path, compress=True)
+            shutil.rmtree(path)  # remove dir and all contains
